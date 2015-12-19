14bc39ab75036847d9c59b49f3f4d1e21f8cec31
YaroslavLitvinenko <L.yaroslav.95@gmail.com>
2015-12-19 14:46:19 +0200

    Трей с AWT и Swing рабочий

--- src/com/himmel/graduate/code/Main.java
+++ src/com/himmel/graduate/code/Main.java
@@ -1,9 +1,62 @@
 package com.himmel.graduate.code;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.*;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("World!!");
+    public static final String APPLICATION_NAME = "TrayIconExample";
+    public static final String ICON_STR = "/images/image.png";
 
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createGUI();
+            }
+        });
+
+    }
+
+    private static void createGUI() {
+        JFrame frame = new JFrame(APPLICATION_NAME);
+        frame.setMinimumSize(new Dimension(300, 200));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+        setTrayIcon();
+    }
+
+    private static void setTrayIcon() {
+        if(! SystemTray.isSupported() ) {
+            return;
+        }
+
+        PopupMenu trayMenu = new PopupMenu();
+        MenuItem item = new MenuItem("Exit");
+        item.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        trayMenu.add(item);
+
+        URL imageURL = Main.class.getResource(ICON_STR);
+
+        Image icon = Toolkit.getDefaultToolkit().getImage(imageURL);
+        TrayIcon trayIcon = new TrayIcon(icon, APPLICATION_NAME, trayMenu);
+        trayIcon.setImageAutoSize(true);
+
+        SystemTray tray = SystemTray.getSystemTray();
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+
+        trayIcon.displayMessage(APPLICATION_NAME, "Application started!", TrayIcon.MessageType.INFO);
     }
 }
