116b1c7ea628fe1aab0a1b5c8b75fd659fe42309
YaroslavLitvinenko <L.yaroslav.95@gmail.com>
2015-12-19 15:22:50 +0200

    Трей с AWT и JavaFX рабочий

---
+++ src/com/himmel/graduate/code/Controller.java
@@ -0,0 +1,16 @@
+package com.himmel.graduate.code;
+
+import javafx.event.ActionEvent;
+
+/**
+ * Created by Lyaro on 19.12.2015.
+ */
+public class Controller {
+    public void File(ActionEvent actionEvent) {
+
+    }
+
+    public void Close(ActionEvent actionEvent) {
+
+    }
+}
--- src/com/himmel/graduate/code/Main.java
+++ src/com/himmel/graduate/code/Main.java
@@ -1,12 +1,18 @@
 package com.himmel.graduate.code;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.*;
 
-public class Main {
+public class Main extends Application {
     public static final String APPLICATION_NAME = "TrayIconExample";
     public static final String ICON_STR = "/com/himmel/graduate/images/image.png";
 
@@ -14,18 +20,19 @@ public class Main {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                createGUI();
+                setTrayIcon();
             }
         });
-
+        launch(args);
     }
 
     private static void createGUI() {
+        /*
         JFrame frame = new JFrame(APPLICATION_NAME);
         frame.setMinimumSize(new Dimension(300, 200));
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setVisible(true);
+        frame.setVisible(true);*/
 
         setTrayIcon();
     }
@@ -59,5 +66,13 @@ public class Main {
         }
 
         trayIcon.displayMessage(APPLICATION_NAME, "Application started!", TrayIcon.MessageType.INFO);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Client");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
     }
 }
